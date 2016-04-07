e7e37617dfafc21b1ccedf51834b373946f769ae
dignifiedquire <dignifiedquire@gmail.com>
2016-04-07 11:51:03 +0200

    Update deps and add support for cbor

--- config/webpack.js
+++ config/webpack.js
@@ -53,6 +53,36 @@ const shared = {
       loader: 'babel',
       query: babel
     }, {
+      test: /\.js$/,
+      include: /node_modules\/cbor/,
+      loader: 'babel',
+      query: {
+        plugins: [
+          // All things supported by node >= 4.0
+          'babel-plugin-transform-es2015-template-literals',
+          'babel-plugin-transform-es2015-literals',
+          'babel-plugin-transform-es2015-function-name',
+          'babel-plugin-transform-es2015-arrow-functions',
+          'babel-plugin-transform-es2015-block-scoped-functions',
+          'babel-plugin-transform-es2015-classes',
+          'babel-plugin-transform-es2015-object-super',
+          'babel-plugin-transform-es2015-shorthand-properties',
+          'babel-plugin-transform-es2015-duplicate-keys',
+          'babel-plugin-transform-es2015-computed-properties',
+          'babel-plugin-transform-es2015-for-of',
+          // 'babel-plugin-transform-es2015-sticky-regex',
+          // 'babel-plugin-transform-es2015-unicode-regex',
+          'babel-plugin-check-es2015-constants',
+          // 'babel-plugin-transform-es2015-spread',
+          // 'babel-plugin-transform-es2015-parameters',
+          // 'babel-plugin-transform-es2015-destructuring',
+          'babel-plugin-transform-es2015-block-scoping',
+          'babel-plugin-transform-es2015-typeof-symbol',
+          // 'babel-plugin-transform-es2015-modules-commonjs',
+          'babel-plugin-transform-regenerator'
+        ].map((p) => require.resolve(p))
+      }
+    }, {
       test: /\.json$/,
       loader: 'json'
     }],
@@ -75,7 +105,7 @@ const shared = {
 }
 
 const dev = _.defaultsDeep({}, shared, {
-  devtool: 'eval',
+  devtool: 'source-map',
   debug: true
 }, specific)
 

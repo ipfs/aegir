97577a3bdfd5fadf071dd27d9f09a145b007907b
dignifiedquire <dignifiedquire@gmail.com>
2016-04-14 12:50:56 +0200

    fix(build): Copy all files, not only js files when building for node

--- tasks/build/node.js
+++ tasks/build/node.js
@@ -1,15 +1,30 @@
 'use strict'
 
 const $ = require('gulp-load-plugins')()
+const runSequence = require('run-sequence')
 
 const config = require('../../config/babel')
 
 module.exports = {
   dep: ['clean:node'],
   fn (gulp, done) {
-    return gulp.src('src/**/*.js')
-      .pipe($.babel(config))
-      .pipe($.size())
-      .pipe(gulp.dest('lib'))
+    gulp.task('copy::node', () => {
+      return gulp.src('src/**/*')
+        .pipe($.size())
+        .pipe(gulp.dest('lib'))
+    })
+
+    gulp.task('build::node', () => {
+      return gulp.src('src/**/*.js')
+        .pipe($.babel(config))
+        .pipe($.size())
+        .pipe(gulp.dest('lib'))
+    })
+
+    runSequence.use(gulp)(
+      'copy::node',
+      'build::node',
+      done
+    )
   }
 }

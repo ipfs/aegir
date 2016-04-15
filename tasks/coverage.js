fdab8658898cf4646e571719feb5ca02ef30f5ce
dignifiedquire <dignifiedquire@gmail.com>
2016-04-15 12:33:00 +0200

    fix(coverage): Run node hooks on coverage task

--- tasks/coverage.js
+++ tasks/coverage.js
@@ -2,17 +2,22 @@
 
 const $ = require('gulp-load-plugins')()
 
+const utils = require('../src/utils')
 const config = require('../config/webpack')
 
 module.exports = {
   fn (gulp, done) {
-    return gulp.src([
-      'test/node.js',
-      'test/**/*.spec.js'
-    ], {read: false})
-      .pipe($.spawnMocha({
-        istanbul: true,
-        timeout: config.dev.timeout
-      }))
+    gulp.task('istanbul', () => {
+      return gulp.src([
+        'test/node.js',
+        'test/**/*.spec.js'
+      ], {read: false})
+        .pipe($.spawnMocha({
+          istanbul: true,
+          timeout: config.dev.timeout
+        }))
+    })
+
+    utils.hooksRun(gulp, 'test:node', ['istanbul'], utils.exitOnFail(done))
   }
 }

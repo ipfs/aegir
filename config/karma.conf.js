7af165fb8d99904dee8f664b1df5995e6f057d98
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-10-24 13:57:07 +0200

    feat(test): add sauce labs support

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,10 +4,70 @@ const path = require('path')
 const webpackConfig = require('./webpack')
 const timeout = require('./custom').timeout
 
-const browsers = []
+let concurrency = 1
+let reporters = ['mocha-own']
+
+const launchers = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'Chrome',
+    platform: 'Windows 10',
+    version: '53'
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'Firefox',
+    platform: 'Windows 10',
+    version: '48'
+  },
+  sl_safari: {
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    platform: 'OS X 10.11',
+    version: 'latest'
+  },
+  sl_ie_11: {
+    base: 'SauceLabs',
+    browserName: 'Internet Explorer',
+    platform: 'Windows 10',
+    version: '11'
+  },
+  sl_edge: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '14.14393'
+  },
+  sl_android: {
+    base: 'SauceLabs',
+    browserName: 'Browser',
+    deviceName: 'Android Emulator',
+    platformName: 'Android',
+    appiumVersion: '1.5.3',
+    platformVersion: '5.1',
+    deviceOrientation: 'portrait'
+  },
+  sl_iphone: {
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    deviceName: 'iPhone Simulator',
+    platformName: 'iOS',
+    platformVersion: '9.3',
+    appiumVersion: '1.5.3',
+    deviceOrientation: 'portrait'
+  }
+}
+
+let browsers = []
 
 if (process.env.TRAVIS) {
-  browsers.push('Firefox')
+  if (process.env.SAUCE_USERNAME) {
+    browsers = Object.keys(launchers)
+    concurrency = 2
+    reporters = ['progress', 'saucelabs']
+  } else {
+    browsers.push('Firefox')
+  }
 } else {
   browsers.push('Chrome')
 }
@@ -34,7 +94,7 @@ module.exports = function (config) {
     webpackMiddleware: {
       noInfo: true
     },
-    reporters: ['mocha-own'],
+    reporters: reporters,
     mochaOwnReporter: {
       reporter: 'spec'
     },
@@ -43,8 +103,9 @@ module.exports = function (config) {
     logLevel: process.env.DEBUG ? config.LOG_DEBUG : config.LOG_INFO,
     autoWatch: false,
     browsers: browsers,
+    customLaunchers: launchers,
     singleRun: false,
-    concurrency: 1,
+    concurrency: concurrency,
     browserNoActivityTimeout: timeout,
     failOnEmptyTestSuite: false
   })
--- tasks/test/browser.js
+++ tasks/test/browser.js
@@ -8,10 +8,16 @@ const utils = require('../../src/utils')
 module.exports = (gulp) => {
   gulp.task('karma', (done) => {
     const debug = process.env.DEBUG
+    const sauce = process.env.SAUCE_USERNAME && process.env.TRAVIS
+
     new Server({
-      configFile: path.join(__dirname, '/../../config/karma.conf.js'),
+      configFile: path.join(__dirname, '../../config/karma.conf.js'),
       singleRun: !debug
     }, (code) => {
+      if (sauce) {
+        // don't fail on saucelabs tests
+        return done()
+      }
       done(code > 0 ? 'Some tests are failing' : undefined)
     }).start()
   })

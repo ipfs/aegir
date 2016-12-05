0ac197abc84b6528fec6c9bcb4e66c1c7b649bf3
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-05 13:45:54 +0100

    prep for documentation.js

--- tasks/docs.js
+++ tasks/docs.js
@@ -1,15 +1,30 @@
 'use strict'
 
-const config = require('../config/jsdoc.js')
+const pump = require('pump')
+const join = require('path').join
+
+const config = require('../config/jsdoc')
+const pkg = require('../config/user').pkg
 
 module.exports = (gulp) => {
-  const jsdoc = require('gulp-jsdoc3')
+  const docs = require('gulp-documentation')
   require('./clean')(gulp)
 
   gulp.task('docs', ['clean:docs'], (cb) => {
-    gulp.src([
-      'README.md', './src/**/*.js'
-    ], {read: false})
-      .pipe(jsdoc(config, cb))
+    pump(
+      gulp.src([
+        './src/**/*.js'
+      ]),
+      docs('html', {
+        config: join(process.cwd(), 'documentation.yml'),
+        github: true
+      }, {
+        theme: require.resolve('clean-documentation-theme'),
+        version: pkg.version,
+        name: pkg.name
+      }),
+      gulp.dest('./docs'),
+      cb
+    )
   })
 }

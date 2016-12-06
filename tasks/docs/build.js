2b9f4fbce5e0904241ab92888d3c8a91ab086134
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-06 14:59:33 +0100

    update deps and only use docs config if it exists

--- tasks/docs/build.js
+++ tasks/docs/build.js
@@ -3,19 +3,26 @@
 const pump = require('pump')
 const join = require('path').join
 const docs = require('gulp-documentation')
+const exists = require('path-exists')
 
 const pkg = require('../../config/user').pkg
 
 module.exports = (gulp) => {
   gulp.task('docs:build', ['clean:docs'], (cb) => {
+    const options = {
+      github: true
+    }
+
+    const configFile = join(process.cwd(), 'documentation.yml')
+    if (exists(configFile)) {
+      options.config = configFile
+    }
+
     pump(
       gulp.src([
         './src/**/*.js'
       ]),
-      docs('html', {
-        config: join(process.cwd(), 'documentation.yml'),
-        github: true
-      }, {
+      docs('html', options, {
         theme: require.resolve('clean-documentation-theme'),
         version: pkg.version,
         name: pkg.name

2d0fba064478a4920e54fb7d53709dee4edbb7cc
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-12-13 12:55:12 +0100

    feat(docs): add generic intro template

--- tasks/docs/build.js
+++ tasks/docs/build.js
@@ -4,8 +4,23 @@ const pump = require('pump')
 const join = require('path').join
 const docs = require('gulp-documentation')
 const exists = require('path-exists').sync
+const fs = require('fs')
+const gutil = require('gulp-util')
 
 const pkg = require('../../config/user').pkg
+const introTmpl = require('../../config/intro-template.md')
+
+function generateDescription (name) {
+  let example
+  try {
+    example = fs.readFileSync(join(
+      process.cwd(), 'example.js'
+    )).toString()
+  } catch (err) {
+    gutil.log('Warning: No `example.js` found in the root directory.')
+  }
+  return introTmpl(name, pkg.repository.url, example)
+}
 
 module.exports = (gulp) => {
   gulp.task('docs:build', ['clean:docs'], (cb) => {
@@ -16,6 +31,11 @@ module.exports = (gulp) => {
     const configFile = join(process.cwd(), 'documentation.yml')
     if (exists(configFile)) {
       options.config = configFile
+    } else {
+      options.toc = [{
+        name: 'Intro',
+        description: generateDescription(pkg.name)
+      }]
     }
 
     pump(

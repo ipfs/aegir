079210bc05482d72c4ab6dcaa2b851fa048e0b71
dryajov <dryajov@gmail.com>
2017-01-28 21:39:03 -0800

    removing reduntant task

--- tasks/test/browser.js
+++ tasks/test/browser.js
@@ -36,10 +36,6 @@ module.exports = (gulp) => {
       utils.exitOnFail(done)
     )
   })
-
-  gulp.task('test:webworker', (done) => {
-    utils.hooksRun(gulp, 'test:browser', ['karma'], utils.exitOnFail(done))
-  })
 }
 
 function karmaTest (webWorker, done) {

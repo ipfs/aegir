fd0a6d81dbef0978c38ae3cde8d9f9e8cc650c98
dignifiedquire <dignifiedquire@gmail.com>
2017-07-07 20:05:21 +0200

    feat: add update-notifier

--- cli.js
+++ cli.js
@@ -2,6 +2,14 @@
 
 'use strict'
 
+const updateNotifier = require('update-notifier')
+const pkg = require('./package.json')
+
+updateNotifier({
+  pkg: pkg,
+  isGlobal: false
+}).notify()
+
 require('yargs') // eslint-disable-line
   .env('AEGIR')
   .commandDir('cmds')

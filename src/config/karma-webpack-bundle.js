b7c4f54186361a7154a064220c17687e9401e2fc
Volker Mische <volker.mische@gmail.com>
2018-03-12 17:01:34 +0100

    fix: don't show warning if test/browser.js doesn't exist
    Check if `test/browser.js` exist before webpack is initiated and require
    it only if it really exist. This makes a warning go away that lead to
    a lot of console output.
    
    Fixes #204.

--- src/config/karma-webpack-bundle.js
+++ src/config/karma-webpack-bundle.js
@@ -1,14 +1,12 @@
-/* global TEST_DIR */
+/* global TEST_DIR, TEST_BROWSER_JS */
 // This is webpack specific. It will create a single bundle out of
 // `test/browser.js and all files ending in `.spec.js` within the
 // `test` directory and all its subdirectories.
 'use strict'
 
 // Load test/browser.js if it exists
-try {
-  require(TEST_DIR + '/browser.js')
-} catch (_err) {
-  // Intentionally empty
+if (TEST_BROWSER_JS) {
+  require(TEST_BROWSER_JS)
 }
 
 const testsContext = require.context(TEST_DIR, true, /\.spec\.js$/)

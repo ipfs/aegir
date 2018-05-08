7896c5e91c0f1130bc1ddfdfb586dedb3700f07f
victorbjelkholm <victorbjelkholm@gmail.com>
2018-05-08 18:55:31 +0200

    refactor: fail early if missing codecov token
    Also, upgrades codecov while we're at it
    
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/coverage/providers/codecov.js
+++ src/coverage/providers/codecov.js
@@ -3,6 +3,9 @@
 const codecov = require('codecov/lib/codecov').upload
 
 function upload (filepath) {
+  if (process.env.CODECOV_TOKEN === undefined) {
+    throw new Error('Missing environment variable `CODECOV_TOKEN`')
+  }
   return new Promise((resolve, reject) => {
     codecov({
       options: {

47a00806f0465e3ad1b51da231b2335afa2111d7
achingbrain <alex@achingbrain.net>
2018-05-21 19:25:21 +0100

    feat: enforce package.json dependency version guidelines
    Our JS code guidelines say we should use `~` as a prefix for pre-v1 dependencies.  Sadly npm disagrees and uses `^` by default.
    
    This PR adds a linting step that examines all dependencies and strenuously objects if it sees anything that violates the guidelines.
    
    https://github.com/ipfs/community/blob/master/js-code-guidelines.md#dependency-versions
    
    feat: allow pinned versions for dependencies
    
    also refactors tests to test more scenarios

--- src/lint.js
+++ src/lint.js
@@ -18,7 +18,59 @@ const FILES = [
   '!**/node_modules/**'
 ]
 
-function lint (opts) {
+function checkDependencyVersions () {
+  const checkVersions = (type, pkg, key) => {
+    const badVersions = []
+
+    if (pkg[key]) {
+      Object.keys(pkg[key]).forEach(name => {
+        const version = pkg[key][name]
+
+        if (/^(?!~)([<>=^]{1,2})[0]/.test(version)) {
+          badVersions.push({
+            type,
+            name,
+            version,
+            message: 'development (e.g. < 1.0.0) versions should start with a ~'
+          })
+        }
+
+        if (/^(?!\^)([<>=~]{1,2})[1-9]/.test(version)) {
+          badVersions.push({
+            type,
+            name,
+            version,
+            message: 'stable versions (e.g. >= 1.0.0) should start with a ^'
+          })
+        }
+      })
+    }
+
+    return badVersions
+  }
+
+  return new Promise((resolve, reject) => {
+    const pkg = require(path.join(process.cwd(), 'package.json'))
+    const badVersions = []
+      .concat(checkVersions('Dependency', pkg, 'dependencies'))
+      .concat(checkVersions('Dev dependency', pkg, 'devDependencies'))
+      .concat(checkVersions('Optional dependency', pkg, 'optionalDependencies'))
+      .concat(checkVersions('Peer dependency', pkg, 'peerDependencies'))
+      .concat(checkVersions('Bundled dependency', pkg, 'bundledDependencies'))
+
+    if (badVersions.length) {
+      badVersions.forEach(({type, name, version, message}) => {
+        console.log(`${type} ${name} had version ${version} - ${message}`)
+      })
+
+      return reject(new Error('Dependency version errors'))
+    }
+
+    resolve()
+  })
+}
+
+function runLinter (opts = {}) {
   return new Promise((resolve, reject) => {
     const cli = new CLIEngine({
       useEslintrc: true,
@@ -33,8 +85,16 @@ function lint (opts) {
     if (report.errorCount > 0) {
       return reject(new Error('Lint errors'))
     }
+
     resolve()
   })
+}
+
+function lint (opts) {
+  return Promise.all([
+    runLinter(opts),
+    checkDependencyVersions(opts)
+  ])
 }
 
 module.exports = lint

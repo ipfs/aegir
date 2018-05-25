7703051ab40900a67eb43437b3bbb115261e9053
Volker Mische <volker.mische@gmail.com>
2018-05-25 12:52:28 +0100

    refactor: use ESLint rules directly
    Instead of using the eslint-config-aegir module, use its eslinrc
    file directly. This makes things clearer and less things to maintain.
    
    ESlint needed to be upgraded for standard v11.

---
+++ src/config/eslintrc.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = {
+  extends: 'standard',
+  parserOptions: {
+    sourceType: 'script'
+  },
+  globals: {
+    self: true
+  },
+  rules: {
+    strict: [2, 'safe'],
+    curly: 'error',
+    'block-scoped-var': 2,
+    complexity: 1,
+    'default-case': 2,
+    'dot-notation': 1,
+    'guard-for-in': 1,
+    'linebreak-style': [1, 'unix'],
+    'no-alert': 2,
+    'no-case-declarations': 2,
+    'no-console': 1,
+    'no-constant-condition': 2,
+    'no-continue': 1,
+    'no-div-regex': 2,
+    'no-empty': 1,
+    'no-empty-pattern': 2,
+    'no-extra-semi': 2,
+    'no-implicit-coercion': 2,
+    'no-labels': 2,
+    'no-loop-func': 2,
+    'no-nested-ternary': 1,
+    'no-script-url': 2,
+    'no-warning-comments': 1,
+    'quote-props': [2, 'as-needed'],
+    'require-yield': 2,
+    'max-nested-callbacks': [2, 4],
+    'max-depth': [2, 4],
+    'valid-jsdoc': [2, {
+      requireParamDescription: false,
+      requireReturnDescription: false
+    }]
+  }
+}

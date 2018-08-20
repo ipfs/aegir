c3de8d0054e411436961244a2167cdee3d10ec9c
achingbrain <alex@achingbrain.net>
2018-08-20 15:28:38 +0100

    feat: support one time passwords
    npm supports one time passwords (OTP) for releases.  This PR adds support
    to Aegir for prompting for an OTP if attempting to publish fails and
    a message asking for an OTP is detected in the failure message.
    
    It uses only the verbose listr renderer and not the update renderer
    as the update renderer causes the OTP prompt to disappear.

--- src/release/publish.js
+++ src/release/publish.js
@@ -1,9 +1,18 @@
 'use strict'
 
 const execa = require('execa')
+const prompt = require('prompt-promise')
 
 function publish () {
   return execa('npm', ['publish'])
+    .catch(error => {
+      if (error.toString().includes('provide a one-time password')) {
+        return prompt.password('Enter an npm OTP: ')
+          .then(otp => execa('npm', ['publish', '--otp', otp.trim()]))
+      }
+
+      throw error
+    })
 }
 
 module.exports = publish

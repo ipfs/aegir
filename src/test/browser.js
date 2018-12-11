d74eded3942c3cd37c201895161dbdd979a2f324
Hugo Dias <hugomrdias@gmail.com>
2018-12-11 14:59:45 +0000

    fix: resolve bin with execa

--- src/test/browser.js
+++ src/test/browser.js
@@ -1,17 +1,18 @@
 'use strict'
-const resolveBin = require('resolve-bin')
+// const resolveBin = require('resolve-bin')
 const execao = require('execa-output')
+const path = require('path')
 const { throwError } = require('rxjs')
 const { catchError } = require('rxjs/operators')
 const { hook, fromAegir } = require('../utils')
 
 module.exports = (argv) => {
-  const bin = resolveBin.sync('karma')
+  // const bin = resolveBin.sync('karma')
   const input = argv._.slice(1)
   const watch = argv.watch ? ['--auto-watch', '--no-single-run'] : []
   return hook('browser', 'pre')(argv.userConfig)
     .then(() => {
-      return execao(bin, [
+      return execao('karma', [
         'start',
         ...watch,
         fromAegir('src/config/karma.conf.js'),
@@ -21,7 +22,8 @@ module.exports = (argv) => {
         env: {
           AEGIR_WEBWORKER: argv.webworker,
           NODE_ENV: process.env.NODE_ENV || 'test'
-        }
+        },
+        localDir: path.join(__dirname, '../..')
       })
         .pipe(catchError(err => {
           return throwError(Object.assign(new Error('Oops tests failed!'), {

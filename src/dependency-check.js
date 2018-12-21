fb2272e70b8887c34b9e772302597d0865c0ff9d
Maciej Krüger <mkg20001@gmail.com>
2018-12-21 21:05:29 +0100

    feat: add abillity to specify extra entrypoints for dependency-check

--- src/dependency-check.js
+++ src/dependency-check.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const execa = require('execa')
+const path = require('path')
+const dlv = require('dlv')
 
 const runCommand = async (options) => {
   return execa('dependency-check', ['./package.json', ...options], {
@@ -9,11 +11,13 @@ const runCommand = async (options) => {
 }
 
 module.exports = async (_argv) => { /* eslint-disable no-console */
+  const pkgJSON = require(path.join(process.cwd(), 'package.json'))
+
   console.log('Checking for missing dependencies of the main library:')
-  await runCommand(['--missing', '--no-dev'])
+  await runCommand(['--missing', '--no-dev'].concat(dlv(pkgJSON, 'aegir.dependencyCheck.additionalEntryPoints', [])))
 
   console.log('Checking for unused dependencies of the main library:')
-  await runCommand(['--unused', '--no-dev'])
+  await runCommand(['--unused', '--no-dev'].concat(dlv(pkgJSON, 'aegir.dependencyCheck.additionalEntryPoints', [])))
 
   console.log('Checking for missing dev-dependencies in tests:')
   await runCommand(['--missing', './test/**/*'])

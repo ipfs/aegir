b4582a0fea4faa391adb89fe6c0d76e5058fb31b
Hugo Dias <hugomrdias@gmail.com>
2019-02-01 18:12:13 +0100

    fix: fix preinstall 5

--- remove-hooks.js
+++ remove-hooks.js
@@ -6,8 +6,8 @@ const crypto = require('crypto')
 
 // git-validate common hash for pre-commit and pre-push 3d4e3b719e1f9e93e9d97315bda564ea2f8d8ebcd255ed5a9809e9525281dd9d
 const hooksList = [
-  ['.git/hooks/pre-commit', '3d4e3b719e1f9e93e9d97315bda564ea2f8d8ebcd255ed5a9809e9525281dd9d'],
-  ['.git/hooks/pre-push', '3d4e3b719e1f9e93e9d97315bda564ea2f8d8ebcd255ed5a9809e9525281dd9d']
+  ['../../.git/hooks/pre-commit', '3d4e3b719e1f9e93e9d97315bda564ea2f8d8ebcd255ed5a9809e9525281dd9d'],
+  ['../../.git/hooks/pre-push', '3d4e3b719e1f9e93e9d97315bda564ea2f8d8ebcd255ed5a9809e9525281dd9d']
 ]
 
 hooksList.forEach(([hook, fileHash]) => {

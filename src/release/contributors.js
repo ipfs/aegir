9a0d81fcc5e84145503fb8d8eaa9eb7ca6df9a75
achingbrain <alex@achingbrain.net>
2020-02-19 22:41:30 -0500

    feat: allow enabling/disabling each part of the release process
    Also splits commit/tagging into two separate commands and checks
    for file modifications before committing updated contributor lists.

--- src/release/contributors.js
+++ src/release/contributors.js
@@ -1,18 +1,25 @@
 'use strict'
 
+const git = require('simple-git')(process.cwd())
+const pify = require('pify')
 const execa = require('execa')
 const { getPathToPkg } = require('../utils')
 
 const contributors = async () => {
   await execa('git-authors-cli', ['--print', 'false'])
-  await execa('git', [
-    'commit',
-    getPathToPkg(),
-    '-m',
+
+  const res = await pify(git.status.bind(git))()
+
+  if (!res.modified.length) {
+    return
+  }
+
+  await pify(git.commit.bind(git))(
     'chore: update contributors',
-    '--no-verify',
-    '--allow-empty'
-  ])
+    getPathToPkg(), {
+      '--no-verify': true
+    }
+  )
 }
 
 module.exports = contributors

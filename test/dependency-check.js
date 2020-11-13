0fa16f963cb0ae0a12450cd39dcb8b20a5767b72
achingbrain <alex@achingbrain.net>
2020-11-13 18:04:08 +0000

    chore: fix up .aegir.js access

--- test/dependency-check.js
+++ test/dependency-check.js
@@ -19,52 +19,54 @@ const yargsv = (overrides = {}) => {
   return merge(argv, overrides)
 }
 
+const argv = (input = []) => {
+  return [
+    'node', 'aegir', 'dependency-check'
+  ].concat(input)
+}
+
 describe('dependency check', () => {
   it('should fail for missing deps', async () => {
-    await expect(check(yargsv(), [], {
+    await expect(check(yargsv(), argv(), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/fail')
     })).to.eventually.be.rejectedWith('execa')
   })
 
   it('should pass when there are no missing deps', async () => {
-    await expect(check(yargsv(), [], {
+    await expect(check(yargsv(), argv(), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/pass')
     })).to.eventually.be.fulfilled()
   })
 
   it('should forward options', async () => {
-    await expect(check(yargsv({ '--': ['--unused'] }), [], {
+    await expect(check(yargsv({ '--': ['--unused'] }), argv(), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/pass')
     })).to.eventually.be.rejectedWith('Modules in package.json not used in code: pico')
   })
 
   it('should fail for missing production deps', async () => {
-    await expect(check(yargsv({ productionOnly: true }), [], {
+    await expect(check(yargsv({ productionOnly: true }), argv(), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/fail-prod')
     })).to.eventually.be.rejectedWith('execa')
   })
 
   it('should pass for passed files', async () => {
+    const file = 'derp/foo.js'
+
     await expect(check(yargsv({
-      input: [
-        'derp/foo.js'
-      ]
-    }), [
-      'node', 'aegir', 'dependency-check', 'derp/foo.js'
-    ], {
+      input: [file]
+    }), argv(file), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/pass-certain-files')
     })).to.eventually.be.fulfilled()
   })
 
   it('should pass for passed production files', async () => {
+    const file = 'derp/foo.js'
+
     await expect(check(yargsv({
       productionOnly: true,
-      input: [
-        'derp/foo.js'
-      ]
-    }), [
-      'node', 'aegir', 'dependency-check', 'derp/foo.js'
-    ], {
+      input: [file]
+    }), argv(file), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/pass-certain-files')
     })).to.eventually.be.fulfilled()
   })
@@ -72,8 +74,16 @@ describe('dependency check', () => {
   it('should pass for ignored modules', async () => {
     await expect(check(yargsv({
       ignore: ['execa']
-    }), [], {
+    }), argv(), {
       cwd: path.join(__dirname, 'fixtures/dependency-check/fail')
+    })).to.eventually.be.fulfilled()
+  })
+
+  it('should pass for modules used in .aegir.js', async () => {
+    await expect(check(yargsv({
+      '--': ['--unused']
+    }), argv(['--', '--unused']), {
+      cwd: path.join(__dirname, 'fixtures/dependency-check/with-aegir-config')
     })).to.eventually.be.fulfilled()
   })
 })

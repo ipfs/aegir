444062881fb42ece16b0553c7444a695ae01ba93
Hugo Dias <hugomrdias@gmail.com>
2020-11-19 12:32:25 +0000

    chore: update bundle

--- actions/bundle-size/utils.js
+++ actions/bundle-size/utils.js
@@ -48,42 +48,42 @@ const sizeCheck = async (octokit, context, baseDir) => {
     console.log('Size check for:', pkgName)
     console.log(out.stdout)
 
-    const parts = out.stdout.split('\n')
-    const title = parts[2]
-    await octokit.checks.update(
-      {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        check_run_id: check.data.id,
-        conclusion: 'success',
-        output: {
-          title: title,
-          summary: [parts[0], parts[1]].join('\n')
-        }
-      }
-    )
+    // const parts = out.stdout.split('\n')
+    // const title = parts[2]
+    // await octokit.checks.update(
+    //   {
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    //     check_run_id: check.data.id,
+    //     conclusion: 'success',
+    //     output: {
+    //       title: title,
+    //       summary: [parts[0], parts[1]].join('\n')
+    //     }
+    //   }
+    // )
 
-    await artifact.create().uploadArtifact(
-      `${pkgName}-size`,
-      await globby(['dist/*'], { cwd: baseDir, absolute: true }),
-      baseDir,
-      {
-        continueOnError: true
-      }
-    )
+    // await artifact.create().uploadArtifact(
+    //   `${pkgName}-size`,
+    //   await globby(['dist/*'], { cwd: baseDir, absolute: true }),
+    //   baseDir,
+    //   {
+    //     continueOnError: true
+    //   }
+    // )
   } catch (err) {
-    await octokit.checks.update(
-      {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        check_run_id: check.data.id,
-        conclusion: 'failure',
-        output: {
-          title: err.stderr ? err.stderr : 'Error',
-          summary: err.stdout ? err.stdout : err.message
-        }
-      }
-    )
+    // await octokit.checks.update(
+    //   {
+    //     owner: context.repo.owner,
+    //     repo: context.repo.repo,
+    //     check_run_id: check.data.id,
+    //     conclusion: 'failure',
+    //     output: {
+    //       title: err.stderr ? err.stderr : 'Error',
+    //       summary: err.stdout ? err.stdout : err.message
+    //     }
+    //   }
+    // )
     throw err
   }
 }

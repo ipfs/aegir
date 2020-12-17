ea8ad984653c3606f6931ed6d7dd4ed951008e79
Hugo Dias <hugomrdias@gmail.com>
2020-12-17 18:34:53 +0000

    chore: chai plugins

--- utils/chai.js
+++ utils/chai.js
@@ -3,11 +3,8 @@
 const chai = require('chai')
 
 // Do not reorder these statements - https://github.com/chaijs/chai/issues/1298
-// @ts-ignore
 chai.use(require('chai-as-promised'))
-// @ts-ignore
 chai.use(require('dirty-chai'))
-// @ts-ignore
 chai.use(require('chai-subset'))
 
 module.exports = {

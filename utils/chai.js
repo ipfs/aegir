6b22723aedd6c0167b116c62142a22a10fecd6ce
achingbrain <alex@achingbrain.net>
2021-04-27 10:09:49 +0100

    chore: chai-parentheses with types

--- utils/chai.js
+++ utils/chai.js
@@ -4,7 +4,6 @@ const chai = require('chai')
 
 // Do not reorder these statements - https://github.com/chaijs/chai/issues/1298
 chai.use(require('chai-as-promised'))
-// @ts-ignore no types
 chai.use(require('chai-parentheses'))
 chai.use(require('chai-subset'))
 chai.use(require('chai-bytes'))

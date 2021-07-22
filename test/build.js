ab5f546ec06fd2df477b7b7695a342933dbc6462
Vasco Santos <vasco.santos@moxy.studio>
2021-07-22 19:13:42 +0200

    chore: skip windows build

--- test/build.js
+++ test/build.js
@@ -3,40 +3,43 @@
 
 const { expect } = require('../utils/chai')
 const execa = require('execa')
+const process = require('process')
 const { copy, existsSync, readJson } = require('fs-extra')
 const { join } = require('path')
 const bin = require.resolve('../')
 const tempy = require('tempy')
 
 describe('build', () => {
-  describe('esm', () => {
-    let projectDir = ''
+  if (process.platform !== 'win32') {
+    describe('esm', () => {
+      let projectDir = ''
 
-    before(async () => {
-      projectDir = tempy.directory()
+      before(async () => {
+        projectDir = tempy.directory()
 
-      await copy(join(__dirname, 'fixtures', 'esm', 'an-esm-project'), projectDir)
-    })
-
-    it('should build an esm project', async function () {
-      this.timeout(20 * 1000) // slow ci is slow
-
-      await execa(bin, ['build'], {
-        cwd: projectDir
+        await copy(join(__dirname, 'fixtures', 'esm', 'an-esm-project'), projectDir)
       })
 
-      expect(existsSync(join(projectDir, 'dist', 'esm'))).to.be.true()
-      expect(existsSync(join(projectDir, 'dist', 'cjs'))).to.be.true()
+      it('should build an esm project', async function () {
+        this.timeout(20 * 1000) // slow ci is slow
 
-      const module = require(join(projectDir, 'dist'))
+        await execa(bin, ['build'], {
+          cwd: projectDir
+        })
 
-      expect(module).to.have.property('useHerp').that.is.a('function')
-      expect(module).to.have.property('useDerp').that.is.a('function')
+        expect(existsSync(join(projectDir, 'dist', 'esm'))).to.be.true()
+        expect(existsSync(join(projectDir, 'dist', 'cjs'))).to.be.true()
 
-      const distPkgPath = join(projectDir, 'dist', 'package.json')
-      const distPkg = await readJson(distPkgPath)
+        const module = require(join(projectDir, 'dist'))
 
-      expect(distPkg.types.includes('dist')).to.be.false()
+        expect(module).to.have.property('useHerp').that.is.a('function')
+        expect(module).to.have.property('useDerp').that.is.a('function')
+
+        const distPkgPath = join(projectDir, 'dist', 'package.json')
+        const distPkg = await readJson(distPkgPath)
+
+        expect(distPkg.types.includes('dist')).to.be.false()
+      })
     })
-  })
+  }
 })

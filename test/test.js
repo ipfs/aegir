5439e777c8492694eaec0f6592e33ba30ceefffc
achingbrain <alex@achingbrain.net>
2022-04-09 17:45:00 +0100

    chore: link project

--- test/test.js
+++ test/test.js
@@ -5,7 +5,6 @@ import fs, { copy } from 'fs-extra'
 import path, { join } from 'path'
 import tempy from 'tempy'
 import { fileURLToPath } from 'url'
-import os from 'os'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -28,29 +27,10 @@ async function setUpProject (project) {
     await fs.createSymlink(path.join(nodeModulesPath, entry), path.join(projectDir, 'node_modules', entry), 'dir')
   }
 
-  // symlink aegir
-  await fs.createSymlink(path.resolve(__dirname, '..'), path.join(projectDir, 'node_modules/aegir'), 'dir')
-
-  // simulate aegir linked in node_modules/.bin
-  if (os.platform() === 'win32') {
-    await fs.writeFile(path.join(projectDir, 'node_modules/.bin/aegir.cmd'), `#!/bin/sh
-basedir=$(dirname "$(echo "$0" | sed -e 's,\\,/,g')")
-
-case \`uname\` in
-    *CYGWIN*|*MINGW*|*MSYS*) basedir=\`cygpath -w "$basedir"\`;;
-esac
-
-if [ -x "$basedir/node" ]; then
-  exec "$basedir/node"  "$basedir/../aegir/src/index" "$@"
-else
-  exec node  "$basedir/../aegir/src/index" "$@"
-fi`)
-    await fs.chmod(path.join(projectDir, 'node_modules/.bin/aegir.cmd'), 0o755)
-  } else {
-    await fs.createSymlink(path.resolve(__dirname, '../src/index.js'), path.join(projectDir, 'node_modules/.bin/aegir'), 'file')
-    await fs.chmod(path.resolve(__dirname, '../src/index.js'), 0o755)
-    await fs.chmod(path.join(projectDir, 'node_modules/.bin/aegir'), 0o755)
-  }
+  // link aegir into project
+  await execa('npm', ['link', path.resolve(__dirname, '..')], {
+    cwd: projectDir
+  })
 
   return projectDir
 }

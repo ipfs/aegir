57e6fffc463b3cb1c444ce7bb98aacb9c019b2ae
Alex Potsides <alex@achingbrain.net>
2022-04-11 13:13:21 +0100

    fix: make resolve node-only (#962)
    Resolve requires use of node features so add browser overrides to ensure it's not pulled in during bundling.

---
+++ utils/resolve.browser.js
@@ -0,0 +1,9 @@
+/**
+ * Returns the full path to the requested resource, if available
+ *
+ * @param {string} filePath
+ * @param {string} [module]
+ */
+export default function resolve (filePath, module = '') {
+  throw new Error('Not supported in the browser')
+}

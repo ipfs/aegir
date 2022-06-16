afe8185211050b14559ba0bfd59860fa8707c4f5
achingbrain <alex@achingbrain.net>
2022-06-16 19:49:39 +0100

    feat: add multiformats readme parts

--- src/check-project/readme/license.js
+++ src/check-project/readme/license.js
@@ -28,5 +28,17 @@ Feel free to join in. All welcome. Open an [issue](https://github.com/ipfs/js-ip
 This repository falls under the IPFS [Code of Conduct](https://github.com/ipfs/community/blob/master/code-of-conduct.md).
 
 [![](https://cdn.rawgit.com/jbenet/contribute-ipfs-gif/master/img/contribute.gif)](https://github.com/ipfs/community/blob/master/CONTRIBUTING.md)
-`
+`,
+  multiformats: `
+## License
+
+Licensed under either of
+
+  * Apache 2.0, ([LICENSE-APACHE](LICENSE-APACHE) / http://www.apache.org/licenses/LICENSE-2.0)
+  * MIT ([LICENSE-MIT](LICENSE-MIT) / http://opensource.org/licenses/MIT)
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+`,
 }

ee1bd62a8d82bf93de2c0e862e2f469529b0ae41
achingbrain <alex@achingbrain.net>
2022-07-07 14:00:46 +0200

    fix: add fallback for project license and badges
    When the repo owner doesn't have a specific type of badge set or
    license, fall back to a default version.

--- src/check-project/readme/license.js
+++ src/check-project/readme/license.js
@@ -1,18 +1,6 @@
 
 /** @type {Record<string, string>} */
 export const LICENSE = {
-  libp2p: `
-## License
-
-Licensed under either of
-
-  * Apache 2.0, ([LICENSE-APACHE](LICENSE-APACHE) / http://www.apache.org/licenses/LICENSE-2.0)
-  * MIT ([LICENSE-MIT](LICENSE-MIT) / http://opensource.org/licenses/MIT)
-
-## Contribution
-
-Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
-`,
   ipfs: `
 ## License
 
@@ -29,7 +17,7 @@ This repository falls under the IPFS [Code of Conduct](https://github.com/ipfs/c
 
 [![](https://cdn.rawgit.com/jbenet/contribute-ipfs-gif/master/img/contribute.gif)](https://github.com/ipfs/community/blob/master/CONTRIBUTING.md)
 `,
-  multiformats: `
+  default: `
 ## License
 
 Licensed under either of

2514ebf91396db9962357f0498bcdf701bc12079
Alex Potsides <alex@achingbrain.net>
2022-12-16 10:32:35 +0000

    fix: update contribution header in footer (#1147)

--- src/check-project/readme/license.js
+++ src/check-project/readme/license.js
@@ -30,7 +30,7 @@ Licensed under either of
   * Apache 2.0, ([LICENSE-APACHE](LICENSE-APACHE) / http://www.apache.org/licenses/LICENSE-2.0)
   * MIT ([LICENSE-MIT](LICENSE-MIT) / http://opensource.org/licenses/MIT)
 
-## Contribute
+## Contribution
 
 Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
 `

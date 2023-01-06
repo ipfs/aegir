c7335e9fe1914309ca1cfa751d3c139ce920d1d3
achingbrain <alex@achingbrain.net>
2023-01-06 13:30:30 +0000

    fix: update monorepo detection and use
    Remove assumption that monorepos use lerna

--- src/check-project/check-monorepo-files.js
+++ src/check-project/check-monorepo-files.js
@@ -15,20 +15,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export async function checkMonorepoFiles (projectDir) {
   console.info('Check monorepo files')
 
-  const pkg = fs.readJSONSync(path.join(projectDir, 'package.json'))
-
-  let defaultLernaContent = fs.readFileSync(path.join(__dirname, 'files/lerna.json'), {
-    encoding: 'utf-8'
-  })
-
-  // ensure lerna version is in sync
-  const lernaConfig = JSON.parse(defaultLernaContent)
-  lernaConfig.lerna = pkg.dependencies.lerna.replace(/\^/, '')
-
-  defaultLernaContent = JSON.stringify(lernaConfig, null, 2)
-
-  await ensureFileHasContents(projectDir, 'lerna.json', defaultLernaContent)
-
   // disable package-lock.json in monorepos until https://github.com/semantic-release/github/pull/487 is merged
   await ensureFileHasContents(projectDir, '.npmrc', fs.readFileSync(path.join(__dirname, 'files/npmrc'), {
     encoding: 'utf-8'

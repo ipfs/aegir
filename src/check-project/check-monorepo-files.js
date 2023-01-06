0bba0e48c12e0698f6bd16fe54f87edf0de8e091
achingbrain <alex@achingbrain.net>
2023-01-06 18:02:46 +0000

    fix: do not create npmrc for monorepos
    We had an npmrc to work around a bug in lerna.  Now that lerna can
    be removed from monorepos we don't need to create the npmrc any more.

--- src/check-project/check-monorepo-files.js
+++ src/check-project/check-monorepo-files.js
@@ -1,22 +1,11 @@
 /* eslint-disable no-console */
 
-import fs from 'fs-extra'
-import path from 'path'
-import {
-  ensureFileHasContents
-} from './utils.js'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 /**
  * @param {string} projectDir
  */
 export async function checkMonorepoFiles (projectDir) {
   console.info('Check monorepo files')
 
-  // disable package-lock.json in monorepos until https://github.com/semantic-release/github/pull/487 is merged
-  await ensureFileHasContents(projectDir, '.npmrc', fs.readFileSync(path.join(__dirname, 'files/npmrc'), {
-    encoding: 'utf-8'
-  }))
+  // we don't need any special files since npm has workspace support,
+  // but if we did, they'd be set up here
 }

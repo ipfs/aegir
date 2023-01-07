b639b2ef280f6d661243a1b00fbb7c98e983531f
Alex Potsides <alex@achingbrain.net>
2023-01-07 07:31:23 +0000

    docs: document env export (#1158)
    Adds typedoc to env export

--- src/env.js
+++ src/env.js
@@ -1,1 +1,17 @@
+/**
+ * @packageDocumentation
+ *
+ * Environment detection for test runs using {@link https://npmjs.com/package/wherearewe wherearewe}
+ *
+ * @example
+ *
+ * ```js
+ * import { isNode } from 'aegir/env'
+ *
+ * if (isNode) {
+ *   // do node things
+ * }
+ * ```
+ */
+
 export * from 'wherearewe'

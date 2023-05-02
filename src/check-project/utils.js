da0df842cf2f21ef454f4c3e3f96909e357577b8
Alex Potsides <alex@achingbrain.net>
2023-05-02 08:15:17 +0100

    chore: update required versions

--- src/check-project/utils.js
+++ src/check-project/utils.js
@@ -247,8 +247,8 @@ export function constructManifest (manifest, manifestFields, repoUrl, homePage =
     },
     keywords: manifest.keywords ? manifest.keywords.sort() : undefined,
     engines: {
-      node: '>=18',
-      npm: '>=9'
+      node: '>=18.0.0',
+      npm: '>=8.6.0'
     },
     bin: manifest.bin,
     ...manifestFields,

765304f92f2c68b673a6627d4403c66a6b21d571
chad <chad.nehemiah94@gmail.com>
2023-05-22 18:08:00 -0500

    tests: skip tests on windows until ts-doc-verifier issue is resolved  (#1273)

--- test/document-check.js
+++ test/document-check.js
@@ -9,29 +9,33 @@ import { expect } from '../utils/chai.js'
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const bin = require.resolve('../src/index.js')
+const isWindows = process.platform === 'win32'
 
 describe('document check', () => {
-  it('should fail for errant typescript in docs', async () => {
-    const cwd = path.join(__dirname, 'fixtures/document-check/ts-fail')
+  // Skip tests on windows until https://github.com/bbc/typescript-docs-verifier/issues/26 is resolved
+  if (!isWindows) {
+    it('should fail for errant typescript in docs', async () => {
+      const cwd = path.join(__dirname, 'fixtures/document-check/ts-fail')
 
-    await expect(execa(bin, ['doc-check', '--inputFiles', `${cwd}/*.md`, '--tsConfigPath', `${cwd}`]))
-      .to.eventually.be.rejected()
-      .with.property('stdout')
-      .that.include('Error compiling example code block 1')
-  })
+      await expect(execa(bin, ['doc-check', '--inputFiles', `${cwd}/*.md`, '--tsConfigPath', `${cwd}`]))
+        .to.eventually.be.rejected()
+        .with.property('stdout')
+        .that.include('Error compiling example code block 1')
+    })
 
-  it('should pass for correct typescript in docs', async () => {
-    const cwd = path.join(__dirname, 'fixtures/document-check/pass')
+    it('should pass for correct typescript in docs', async () => {
+      const cwd = path.join(__dirname, 'fixtures/document-check/pass')
 
-    await expect(execa(bin, ['doc-check', '--inputFiles', `${cwd}/*.md`])).to.eventually.be.fulfilled()
-  })
+      await expect(execa(bin, ['doc-check', '--inputFiles', `${cwd}/*.md`])).to.eventually.be.fulfilled()
+    })
 
-  it('should fail for missing tsconfig.json', async () => {
-    const cwd = path.join(__dirname, 'fixtures/document-check/tsconfig-fail')
+    it('should fail for missing tsconfig.json', async () => {
+      const cwd = path.join(__dirname, 'fixtures/document-check/tsconfig-fail')
 
-    await expect(execa(bin, ['doc-check', '--inputFiles', `${cwd}/*.md`, '--tsConfigPath', `${cwd}`]))
-      .to.eventually.be.rejected()
-      .with.property('stderr')
-      .that.include('no such file or directory')
-  })
+      await expect(execa(bin, ['doc-check', '--inputFiles', `${cwd}/*.md`, '--tsConfigPath', `${cwd}`]))
+        .to.eventually.be.rejected()
+        .with.property('stderr')
+        .that.include('no such file or directory')
+    })
+  }
 })

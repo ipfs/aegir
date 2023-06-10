cbe463bf7108ac1528c682c1e4aa0af81328b342
achingbrain <alex@achingbrain.net>
2023-06-10 08:14:24 +0200

    test: test on lts and current

--- src/cmds/test.js
+++ src/cmds/test.js
@@ -107,6 +107,12 @@ export default {
    * @param {any} argv
    */
   async handler (argv) {
+    // temporarily disable code coverage on node 18
+    if (argv.cov && process.version.startsWith('v18.')) {
+      console.warn(kleur.red('!!! Temporarily disabling code coverage - https://github.com/ipfs/aegir/issues/1206\n')) // eslint-disable-line no-console
+      delete argv.cov
+    }
+
     await testCmd.run({
       ...argv,
       bundle: false

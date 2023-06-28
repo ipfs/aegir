8150bdf8ab4d706a2aa4b4f79ce697634dc61c77
achingbrain <alex@achingbrain.net>
2023-06-28 08:53:00 +0200

    fix: allow specifying ci file in check-project
    Not all projects use js-test-and-release.yml as their main CI file
    so allow specifying something different.

---
+++ src/check-project/manifests/untyped-esm.js
@@ -0,0 +1,56 @@
+import mergeOptions from 'merge-options'
+import { semanticReleaseConfig } from '../semantic-release-config.js'
+import {
+  sortFields,
+  constructManifest
+} from '../utils.js'
+
+const merge = mergeOptions.bind({ ignoreUndefined: true })
+
+/**
+ * @param {any} manifest
+ * @param {string} branchName
+ * @param {string} repoUrl
+ * @param {string} [homePage]
+ */
+export async function untypedESMManifest (manifest, branchName, repoUrl, homePage = repoUrl) {
+  let proposedManifest = constructManifest(manifest, {
+    type: 'module',
+    files: [
+      'src',
+      'dist',
+      '!dist/test',
+      '!**/*.tsbuildinfo'
+    ],
+    exports: {
+      '.': {
+        types: './dist/src/index.d.ts',
+        import: './src/index.js'
+      }
+    },
+    eslintConfig: merge({
+      extends: 'ipfs',
+      parserOptions: {
+        sourceType: 'module'
+      }
+    }, manifest.eslintConfig),
+    release: (manifest.scripts?.release?.includes('semantic-release') || manifest.scripts?.release?.includes('aegir release')) ? semanticReleaseConfig(branchName) : undefined
+  }, repoUrl, homePage)
+
+  const rest = {
+    ...sortFields(manifest)
+  }
+
+  for (const key of Object.keys(proposedManifest)) {
+    delete rest[key]
+  }
+
+  proposedManifest = {
+    ...proposedManifest,
+    ...rest,
+    contributors: undefined,
+    leadMaintainer: undefined
+  }
+
+  return proposedManifest
+}

f78c9ac808fd259be95c660ca12967a408fb0f5b
Cayman <caymannava@gmail.com>
2024-07-29 02:59:48 -0600

    feat: move tsc linter rules to eslint (#1513)
    - Resolves https://github.com/ipfs/aegir/issues/1459
    - Since all relevant typescript linter rules are now incorporated in the latest eslint-config-ipfs, we can drop them from our tsconfig.
    
    ---------
    
    Co-authored-by: Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>

--- test/fixtures/dependency-check/fail-unused/index.js
+++ test/fixtures/dependency-check/fail-unused/index.js
@@ -1,3 +1,2 @@
 /* eslint-disable no-unused-vars */
-// @ts-expect-error unused
 import { execa } from 'execa'
--- test/fixtures/dependency-check/ts-pass/src/index.ts
+++ test/fixtures/dependency-check/ts-pass/src/index.ts
@@ -1,3 +1,2 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// @ts-expect-error unused
 import { execa } from 'execa'

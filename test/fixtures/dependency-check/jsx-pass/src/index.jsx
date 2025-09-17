16ba8c7bd4e3a2e17b470022efe3183a14f9090d
Alex Potsides <alex@achingbrain.net>
2025-09-17 13:24:40 +0100

    fix: time out browser coverage collection (#1865)
    It seems browser coverage collection can hang in the same way node
    coverage does.

--- test/fixtures/dependency-check/jsx-pass/src/index.jsx
+++ test/fixtures/dependency-check/jsx-pass/src/index.jsx
@@ -1,9 +1,9 @@
 // @ts-expect-error - not installed
-// eslint-disable-next-line no-unused-vars
+
 import React from 'react'
 
 // @ts-expect-error - not installed
-// eslint-disable-next-line no-unused-vars
+
 const Component = () => (<div>Hello, world!</div>)
 const App = () => (<Component />)
 

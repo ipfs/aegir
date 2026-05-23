af0d19449d87f14687e51bf48171cdb0447ef1ba
Alex Potsides <alex@achingbrain.net>
2026-05-23 10:48:56 +0100

    deps: update chai-bites (#1960)
    Update to latest version for ESM and failure messages

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 import { toString as uint8ArrayToString } from 'uint8arrays/to-string'
 import { expect } from '../utils/chai.js'
 import loadFixture from '../utils/fixtures.js'
@@ -14,9 +15,10 @@ describe('fixtures', () => {
     expect(uint8ArrayToString(myFixture).trim()).to.be.eql('Hello Fixture')
   })
 
-  it('should load tar.gz fixures', () => {
+  it('should load tar.gz fixtures', () => {
     const myFixture = loadFixture(path.join('test', 'fixtures', 'file.tar.gz'))
     // spell-checker: disable-next-line
     expect(uint8ArrayToString(myFixture, 'base64')).to.equal('H4sICIlTHVIACw')
+    expect(myFixture).to.equalBytes(uint8ArrayFromString('H4sICIlTHVIACw', 'base64'), 'did not load correct file contents')
   })
 })

8ace8a2b4e54a7bc946f7804c7482da81e474231
achingbrain <alex@achingbrain.net>
2026-06-15 11:16:44 +0300

    fix: output is trimmed so fix regex

--- src/test/utils.js
+++ src/test/utils.js
@@ -21,19 +21,19 @@ export async function killIfProcessHangs (proc, argv) {
 
     lastLine = stripAnsi(data.toString()).trim()
 
-    if (lastLine.trim() !== '') {
+    if (lastLine !== '') {
       // more output has been sent, reset timer
       clearTimeout(timeout)
     }
 
-    if (lastLine.match(/^ {2}\d+ (passing|pending)/m) != null) {
+    if (lastLine.match(/^\d+ (passing|pending)/m) != null) {
       // if we see something that looks like the successful end of a mocha
       // run, set a timer - if the process does not exit before the timer
       // fires, kill it and log a warning, though don't cause the test run
       // to fail
       timeout = setTimeout(() => {
         if (argv.cov) {
-          console.warn(kleur.red('!!! Process has while collecting coverage, manually killing it')) // eslint-disable-line no-console
+          console.warn(kleur.red('!!! Process has hung while collecting coverage, manually killing it')) // eslint-disable-line no-console
           console.warn(kleur.red('!!! See https://github.com/ipfs/aegir/issues/1206 for more information')) // eslint-disable-line no-console
         } else {
           console.warn(kleur.red('!!! Process has hung after tests passed, manually killing it')) // eslint-disable-line no-console
